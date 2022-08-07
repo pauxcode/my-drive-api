a3e67914c0adf3c820df781cb82484b5518926b7
Pau Lovell <pauxcode@gmail.com>
2022-08-07 10:33:19 -0500

    IDK

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import fileUpload from 'express-fileupload'
 import cors from 'cors'
+import path from 'path'
 
 export const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload())
